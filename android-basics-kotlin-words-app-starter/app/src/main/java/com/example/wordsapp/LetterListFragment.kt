cd479f6a66e05037cfd83ec37c18b23ea4b430e0
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-13 00:28:47 +0300

    setIcon

--- android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -2,6 +2,7 @@ package com.example.wordsapp
 
 import android.os.Bundle
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.databinding.FragmentLetterListBinding
@@ -42,6 +43,10 @@ class LetterListFragment : Fragment() {
         chooseLayout()
     }
 
+    private fun chooseLayout() {
+        TODO("Not yet implemented")
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -49,6 +54,28 @@ class LetterListFragment : Fragment() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.layout_menu, menu)
+
+        val layoutButton = menu.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+    }
+
+    private fun setIcon(layoutButton: MenuItem?) {
+        if (layoutButton == null)
+            return
+
+        // Set the drawable for the menu icon based on which LayoutManager is currently in use
+
+        // An if-clause can be used on the right side of an assignment if all paths return a value.
+        // The following code is equivalent to
+        // if (isLinearLayoutManager)
+        //     menu.icon = ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+        // else menu.icon = ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+        layoutButton.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+
+
     }
 }
