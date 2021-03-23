4dae59e1ecc044aeb7d939cb873892d3d474d255
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-24 01:09:31 +0300

    configured the WorldListFragmnet

--- android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -30,9 +30,12 @@ class WordListFragment : Fragment() {
 
     private var _binding: FragmentWordListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var letterId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        arguments?.let { letterId = it.getString(LETTER).toString() }
 
     }
 
@@ -48,7 +51,7 @@ class WordListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = WordAdapter(activity?.intent?.extras?.getString(LETTER).toString(), requireContext())
+        recyclerView.adapter = WordAdapter(letterId/*activity?.intent?.extras?.getString(LETTER).toString()*/, requireContext())
 
         recyclerView.addItemDecoration(
             DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
