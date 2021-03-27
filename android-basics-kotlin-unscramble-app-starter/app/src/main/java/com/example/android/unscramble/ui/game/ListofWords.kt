387133ffbb19fc3ca4bd8ef53332178e54238646
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-27 21:53:28 +0300

    SCORE INCREASE = 10

--- android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
+++ android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -18,7 +18,7 @@ package com.example.android.unscramble.ui.game
 
 
 const val MAX_NO_OF_WORDS = 10
-const val SCORE_INCREASE = 20
+const val SCORE_INCREASE = 10
 
 // List with all the words for the Game
 val allWordsList: List<String> =
